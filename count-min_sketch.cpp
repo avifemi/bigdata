c0eaca7cbefaef692389fd27f0103ee623fa5c69
alina <140841165+avifemi@users.noreply.github.com>
2026-05-13 14:36:02 +0300

    Update count-min_sketch.cpp

--- count-min_sketch.cpp
+++ count-min_sketch.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <functional>
 #include <climits>
+
 using namespace std;
 
 class CountMinSketch 
@@ -18,7 +19,8 @@ public:
         int depth,
         int width,
         vector<function<int(const string&, int)>> hashes
-    ) {
+    ) 
+    {
         this->depth = depth;
         this->width = width;
         this->hashFunctions = hashes;
@@ -29,7 +31,7 @@ public:
     {
         for (int i = 0; i < depth; i++) 
         {
-            int idx = hashFunctionsi;
+            int idx = hashFunctions[i](key, width);
             table[i][idx] += count;
         }
     }
@@ -37,24 +39,29 @@ public:
     int estimate(const string& key) 
     {
         int minVal = INT_MAX;
+
         for (int i = 0; i < depth; i++) 
         {
-            int idx = hashFunctionsi;
+            int idx = hashFunctions[i](key, width);
             minVal = min(minVal, table[i][idx]);
         }
+
         return minVal;
     }
 
     void print() 
     {
         cout << "\nCount-Min Sketch:\n";
+
         for (int i = 0; i < depth; i++) 
         {
             cout << "Row " << i << ": ";
+
             for (int j = 0; j < width; j++) 
             {
                 cout << table[i][j] << " ";
             }
+
             cout << endl;
         }
     }
@@ -63,21 +70,24 @@ public:
 int hash1(const string& key, int width) 
 {
     int h = 0;
-    for (char c : key) h = (h * 31 + c) % width;
+    for (char c : key)
+        h = (h * 31 + c) % width;
     return h;
 }
 
 int hash2(const string& key, int width) 
 {
     int h = 0;
-    for (char c : key) h = (h * 17 + c * 7) % width;
+    for (char c : key)
+        h = (h * 17 + c * 7) % width;
     return h;
 }
 
 int hash3(const string& key, int width) 
 {
     int h = 5381;
-    for (char c : key) h = ((h << 5) + h + c) % width;
+    for (char c : key)
+        h = ((h << 5) + h + c) % width;
     return h;
 }
 
@@ -87,15 +97,19 @@ int main()
     {
         hash1, hash2, hash3
     };
+
     CountMinSketch cms(3, 20, hashes);
+
     cms.add("apple");
     cms.add("apple");
     cms.add("banana", 3);
     cms.add("orange", 2);
-    cout << "apple count ≈ " << cms.estimate("apple") << endl;
-    cout << "banana count ≈ " << cms.estimate("banana") << endl;
-    cout << "orange count ≈ " << cms.estimate("orange") << endl;
-    cout << "grape count ≈ " << cms.estimate("grape") << endl;
+
+    cout << "apple count~" << cms.estimate("apple") << endl;
+    cout << "banana count~" << cms.estimate("banana") << endl;
+    cout << "orange count~" << cms.estimate("orange") << endl;
+    cout << "grape count~" << cms.estimate("grape") << endl;
+
     cms.print();
 
     return 0;
